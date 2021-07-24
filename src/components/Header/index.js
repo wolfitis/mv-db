c47c73de796fae624f9bb89acc525efc85768c1a
wolfitis <webblog123@gmail.com>
2021-07-23 23:27:30 -0400

    implement routing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import MVDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -8,7 +9,10 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={MVDBLogo} alt='mv-logo' />
+      {/* similar to a-tag in HTML */}
+      <Link to='/'>
+        <LogoImg src={MVDBLogo} alt='mv-logo' />
+      </Link>
       <TMDBLogoImg src={TMDBLogo} alt='tmbd-logo' />
     </Content>
   </Wrapper>
