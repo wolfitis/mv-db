b4c5cef169b9af0cc6e7c64563c49dcb2453238c
wolfitis <webblog123@gmail.com>
2021-07-24 11:26:41 -0400

    fix Actor component

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Styles
 import { Wrapper, Image } from './Actor.styles';
 
-const Actor = ({ name, character, imageUrl }) = (
+const Actor = ({ name, character, imageUrl }) => (
   <Wrapper>
     <Image src={imageUrl} alt='actor-thumb' />
     <h3>{name}</h3>
