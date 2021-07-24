308cea51d56d5b67d4351525724e75fc0b063e7b
wolfitis <webblog123@gmail.com>
2021-07-24 01:43:10 -0400

    add MovieInfo component

--- src/components/MovieInfo/MovieInfo.styles.js
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -34,4 +34,41 @@ export const Content = styled.div`
   }
 `;
 
-export const Text = styled.div``;
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  rating-directors {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 35px;
+    background: #fff;
+    color: #000;
+    font-weight: 800;
+    border-radius: 50%;
+    margin: 0;
+  }
+
+  director {
+    margin: 0 0 0 40px;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  h1 {
+    @media screen and (max-width: 760px) {
+      font-size: var(--fontBig);
+    }
+  }
+`;
